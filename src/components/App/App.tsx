9eae6e246a765d1ce1cb0081b0a6507d93e761f8
ivshynalarysa <ivshynalarysa@gmail.com>
2025-05-23 14:28:37 +0100

    test

---
+++ src/components/App/App.tsx
@@ -0,0 +1,37 @@
+import css from './App.module.css';
+import CafeInfo from '../App/CafeInfo/CafeInfo';
+
+/*import Notification from '../App/Notification/Notification';
+import VoteOptions from '../App/VoteOptions/VoteOptions';
+import VoteStats from '../App/VoteStats/VoteStats';
+import { useState } from 'react';
+import { Votes, VoteType } from '../../types/votes'*/
+
+export default function App() {
+
+ /* const [votes, SetVotes] = useState<Votes> (
+    {
+	good: 0,
+	neutral: 0,
+	bad: 0
+}
+
+  );
+
+  const handleVote(VoteType) = {}
+
+  const resetVotes() 
+
+*/
+
+  return 
+    <>
+      <div className={css.app}>
+        <CafeInfo />
+      </div>
+
+    </>
+  
+}
+
+
---
+++ src/types/votes.ts
@@ -0,0 +1,11 @@
+export 
+
+interface Votes {
+
+   good: number;
+   neutral: number;
+   bad: number;
+
+}
+
+export type VoteType = 'good' | 'neutral' | 'bad';
